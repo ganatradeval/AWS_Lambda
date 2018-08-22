900aac03362ff8a658e9592baa3b7001942fc04f
Deval <ganatradeval@gmail.com>
2018-08-22 16:52:48 +0530

    Start Stop minor changes

--- Schedule EC2/EC2_Start.py
+++ Schedule EC2/EC2_Start.py
@@ -1,10 +1,10 @@
-import boto3    #Python library for AWS
-# Enter the region your instances are in. e.g., 'us-east-1'
+import boto3    #Python library for AWS 
+# Enter the region your instances are in. e.g.; 'us-east-1'
 region = 'XX-XXXXX-X'
 # Enter your instances here: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
 instances = ['X-XXXXXXXX']
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
-    ec2.stop_instances(InstanceIds=instances)
-    print 'stopped your instances: ' + str(instances)
+    ec2.start_instances(InstanceIds=instances)
+    print 'started your instances: ' + str(instances)
